f15f5c7ad1cb58bbb3872be3984d09bb4acdeb00
APGM82 <90765662+APGM82@users.noreply.github.com>
2022-10-12 23:11:06 +0200

    Con Imagenes

--- app/src/main/java/com/example/dadosapgm/MainActivity.kt
+++ app/src/main/java/com/example/dadosapgm/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -14,6 +15,10 @@ lateinit var txtres2:TextView
 lateinit var bt : Button
 lateinit var bt2 : Button
 lateinit var res:TextView
+//lateinit var img1:ImageView
+lateinit var img2:ImageView
+lateinit var img3:ImageView
+
 
 var resultado1:Int = 0
 var resultado2:Int = 0
@@ -33,12 +38,46 @@ class MainActivity : AppCompatActivity() {
         txtres1=findViewById(R.id.textViewResJ)
         txtres2=findViewById(R.id.textViewResPC)
         res=findViewById(R.id.textViewMensaje)
+        //img1=findViewById(R.id.imageView1)
+        img2=findViewById(R.id.imageView2)
+        img3=findViewById(R.id.imageView3)
+
 
         bt.setOnClickListener {
             contador++
             if (contador<6){
                 resultado1=Random.nextInt(1,7)
+                when (resultado1) {
+                    1 ->
+                        img2.setImageResource(R.drawable.d1)
+                    2 ->
+                        img2.setImageResource(R.drawable.d2)
+                    3 ->
+                        img2.setImageResource(R.drawable.d3)
+                    4 ->
+                        img2.setImageResource(R.drawable.d4)
+                    5 ->
+                        img2.setImageResource(R.drawable.d5)
+                    6 ->
+                        img2.setImageResource(R.drawable.d6)
+                }
+
                 resultado2= Random.nextInt(1,7)
+                when (resultado2) {
+                    1 ->
+                        img3.setImageResource(R.drawable.d1)
+                    2 ->
+                        img3.setImageResource(R.drawable.d2)
+                    3 ->
+                        img3.setImageResource(R.drawable.d3)
+                    4 ->
+                        img3.setImageResource(R.drawable.d4)
+                    5 ->
+                        img3.setImageResource(R.drawable.d5)
+                    6 ->
+                        img3.setImageResource(R.drawable.d6)
+                }
+
                 txtDado1.text=resultado1.toString()
                 txtDado2.text= resultado2.toString()
                 txtres1.text= ((Integer.parseInt(txtres1.text.toString()))+ resultado1).toString()
